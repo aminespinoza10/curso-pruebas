12a582c9472c2657be2b1a360e3e289dd48e1ea0
aminespinoza10 <amin.espinoza@outlook.com>
2022-10-07 16:09:22 -0500

    e2e actualizado

--- Javascript/Unitarias/util.test.js
+++ Javascript/Unitarias/util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateAge, generateNumber, generateMail, checkAndGenerateAge, checkAndGeneratePhone, checkAndGenerateMail } = require('./util')
 
 /*------------------
@@ -34,4 +35,18 @@ test('deberá generar una salida de texto válida para el teléfono', () => {
 test('deberá generar una salida de texto válida para el correo', () => {
     const text = checkAndGenerateMail('Marcela', 'marcemar@microsoft.com');
     expect(text).toBe('Marcela (Tu correo es: marcemar@microsoft.com)')
+});
+
+/*------------------
+Pruebas E2E
+--------------------*/
+test('dando unos cuandos clics', async() => {
+    const explore = await puppeteer.launch({
+        headless: false,
+        slowMo: 80,
+        args: ['--window-size=1920,1080']
+    });
+
+    const page = await explore.newPage();
+    await page.goto('file:///C:/Users/amines/Documents/Github/curso-pruebas/Javascript/Unitarias/index.html');
 });
